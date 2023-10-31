d789938d8ffe0caf03ad77c788f2ac23f9ef6d78
Anushka gupta <104565993+Anushkag031@users.noreply.github.com>
2023-10-31 09:58:30 +0530

    Create rotate-image-matrix.java

---
+++ rotate-image-matrix.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        //transpose of the matrix
+        for(int i=0; i<matrix.length;i++)
+        {
+            for(int j=i; j<matrix[0].length; j++)
+            {
+                int temp=0;
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        //reversing the transpose
+       // for(int i=0; i<matrix.length; i++)
+       // {
+       //     for(int j=0; j<matrix.length/2;j++)
+       //     {
+       //         int temp=matrix[i][j];
+       //         matrix[i][j]=matrix[i][matrix.length-1-j];
+       //         matrix[i][matrix.length-1-j]=temp;
+        //    }
+       // }
+       int col1=0;
+       int col2=matrix.length-1;
+       while(col1<col2)
+       {
+           for(int i=0;i<matrix.length; i++)
+           {
+               int temp=matrix[i][col1];
+               matrix[i][col1]=matrix[i][col2];
+               matrix[i][col2]=temp;
+           }
+           col1++;
+           col2--;
+       }
+    }
+}
